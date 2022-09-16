e2104ddf10de28af307f7f58af6079af24ecafd7
HyeokSeong Kwon <khs50851@naver.com>
2022-09-16 22:26:39 +0900

    delete 테스트

--- src/test/java/site/hyprac/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/hyprac/junitproject/domain/BookRepositoryTest.java
@@ -1,17 +1,35 @@
 package site.hyprac.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 public class BookRepositoryTest {
     
     @Autowired
     private BookRepository bookRepository;
-
+    // @BeforeAll // 테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void 데이터준비() {
+        String title = "junit5";
+        String author = "hs";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    } // 트랜잭션 종료 됐다면 말이안됨
+    // 가정 1 : [ 데이터준비() + 1 책등록 ] (T) , [ 데이터준비() + 2 책목록보기 ] (T) -> 사이즈 1 (검증 완료)
+    // 가정 2 : [ 데이터준비() + 1 책등록 + 데이터준비() + 2 책목록보기 ] (T) -> 사이즈 2 (검증 실패)
     // 1. 책 등록
     @Test
     public void 책등록_test(){
@@ -29,11 +47,51 @@ public class BookRepositoryTest {
         assertEquals(author, bookPS.getAuthor());
     }
     // 2. 책 목록보기
+    @Test
+    public void 책목록보기_test() {
+        // given
+        String title = "junit5";
+        String author = "hs";
+
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println("사이즈 : =============================================      : " + booksPS.size());
+
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책한건보기_test() {
+        // given
+        String title = "junit5";
+        String author = "hs";
 
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
     // 4. 책 수정
 
     // 5. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test() {
+        // given
+        Long id = 1L;
 
+        // when
+        bookRepository.deleteById(id);
+
+        // then
+        assertFalse(bookRepository.findById(id).isPresent());
+       
+    }
 }
